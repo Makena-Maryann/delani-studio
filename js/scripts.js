6dd10dd636c93886101298af7df15516d378f26a
Makena-Maryann <maryann.makena00@gmail.com>
2020-05-10 09:16:15 +0300

    Added a toggle effect to the what-we-do section

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".click1").click(function () {
+    $(".toggle1").toggle();
+  });
+  $(".click2").click(function () {
+    $(".toggle2").toggle();
+  });
+  $(".click3").click(function () {
+    $(".toggle3").toggle();
+  });
+});
