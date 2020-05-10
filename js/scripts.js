50c3122b924d9df2b65301215a4acae59c2de0fc
Makena-Maryann <maryann.makena00@gmail.com>
2020-05-10 10:33:11 +0300

    Added a hover effect to the portfolio section

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,18 @@ $(document).ready(function () {
     $(".toggle1").toggle();
     $(".click1").toggle();
   });
+
   $(".clickable2").click(function () {
     $(".toggle2").toggle();
     $(".click2").toggle();
   });
+
   $(".clickable3").click(function () {
     $(".toggle3").toggle();
     $(".click3").toggle();
+  });
+
+  $(".card-img").hover(function () {
+    $(".project-name").toggle();
   });
 });
